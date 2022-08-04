a5674f19338337b20894f365163213622bc70ac9
cryptosmasher <cryptosmasher@gmail.com>
2022-08-04 14:28:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ bot.onText(TWITTER_URL, (msg, match, error) => {
      // },
     //});
   //});
-});
+}
+                                    //);
 
 // 5. React to inline keyboard reply
 bot.on("callback_query", async (answer) => {
