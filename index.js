1cb66c295ec8a78f82438fa449041faad4743a1d
cryptosmasher <cryptosmasher@gmail.com>
2022-08-04 15:14:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,25 @@ bot.onText(TWITTER_URL, (msg, match, error) => {
   // 3. Get message text to parse links from
   const msgText = msg.text;
 
-const expandedLink = link.replace("twitter.com", "vxtwitter.com");
-  
+  // 4. Iterate through all links in the message
+  msgText.match(TWITTER_URL).forEach((link) => {
+              callback_data: link,
+              // callback_data has a 64 byte limit!!!
+            }
+        
+
+// 5. React to inline keyboard reply
+bot.on("callback_query", async (answer) => {
+  const chatId = answer.message.chat.id;
+  const msgId = answer.message.message_id;
+  const link = answer.data;
+
+  const expandedLink = link.replace("twitter.com", "vxtwitter.com");
+
   // 6b. Replace the reply with an expanded Tweet link
   bot.editMessageText(expandedLink, {
     chat_id: chatId,
     message_id: msgId,
   });
+
 });
